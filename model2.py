a75da8ef7cebbc229492fa333490c06c9d62e476
ljw159357 <748628253@qq.com>
2025-05-02 17:53:13 +0800

    Update model2.py

--- model2.py
+++ model2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import streamlit as st
 import joblib
 import numpy as np
@@ -12,9 +9,19 @@ import shap
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
-
-# In[2]:
-
+# 特征的缩写字典
+feature_abbr = {
+    "Postoperative Platelet Count (x10⁹/L)": "post_plt",
+    "Urgent Postoperative APTT (s)": "post_APTT_u",
+    "Day 1 Postoperative APTT (s)": "post_APTT_1",
+    "Day 1 Postoperative Antithrombin III Activity (%)": "post_antithrombin_III_1",
+    "Postoperative CRRT (Continuous Renal Replacement Therapy)": "post_CRRT",
+    "Postoperative Anticoagulation": "post_anticoagulation",
+    "Transplant Side": "trans_side",
+    "Primary Graft Dysfunction (PGD, Level)": "PGD",
+    "Height": "height",  # 其他特征也可以添加缩写
+    "HBP": "hbp"
+}
 
 # 加载模型
 model = joblib.load('xgb.pkl')
@@ -71,10 +78,7 @@ if numerical_values:
 
 features = np.array([feature_values])
 
-
-# In[3]:
-
-
+# 预测
 if st.button("Predict"):
     predicted_class = model.predict(features)[0]
     predicted_proba = model.predict_proba(features)[0]
@@ -98,33 +102,31 @@ if st.button("Predict"):
 
     tree_model = get_tree_model(model)
     explainer = shap.TreeExplainer(tree_model)
-    # explainer = shap.TreeExplainer(model)
+    
+    
     shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_keys))
+    
+    if isinstance(shap_values, list) and len(shap_values) > 1:
+        shap_values_for_display = shap_values[1]  
+        base_value = explainer.expected_value[1]
+    elif isinstance(shap_values, list) and len(shap_values) == 1:
+        shap_values_for_display = shap_values[0]
+        base_value = explainer.expected_value[0]
+    else:
+        shap_values_for_display = shap_values
+        base_value = explainer.expected_value
 
+    # 将特征名替换为缩写
+    feature_keys_abbr = [feature_abbr.get(f, f) for f in feature_keys]  # 将特征名替换为缩写
+
+    # 生成 SHAP 力图
     shap.initjs()
     shap_fig = shap.plots.force(
-        explainer.expected_value[1],  # 类别 1 的基准值
-        shap_values[0, :, 1],  # 类别 1 的 SHAP 值
-        pd.DataFrame([feature_values], columns=feature_keys),
-        # feature_names=features_adult_en,  # 特征名称
+        base_value,  # 基准值
+        shap_values_for_display,  # SHAP 值
+        pd.DataFrame([feature_values], columns=feature_keys_abbr),  # 使用缩写作为列名
         matplotlib=True,
         show=False  # 不自动显示图形
     )
-    # shap_fig = shap.plots.force(
-    #     # explainer.expected_value[predicted_class],
-    #     # shap_values[predicted_class],
-    #     expected_value,
-    #     shap_values_for_display,
-    #     pd.DataFrame([feature_values], columns=feature_keys),
-    #     matplotlib=True
-    # )
+    
     st.pyplot(shap_fig)
-    # st_shap_html = f"<head>{shap.getjs()}</head><body>{shap.save_html(None, shap_fig, return_html=True)}</body>"
-    # st.components.v1.html(st_shap_html, height=300)
-
-
-# In[ ]:
-
-
-
-
