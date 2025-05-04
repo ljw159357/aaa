9826c4ec879d3f0890402f0463685dbfc2c8fcd5
ljw159357 <748628253@qq.com>
2025-05-04 20:16:49 +0800

    Update model2.py

--- model2.py
+++ model2.py
@@ -50,8 +50,8 @@ category_to_numeric_mapping = {
 }
 
 # UI
-st.title("Prediction Model for Thrombosis After Lung Transplantation")
-st.header("Enter the following feature values:")
+st.header("Prediction Model for Thrombosis After Lung Transplantation")
+st.write("Enter the following feature values:")
 
 feature_values = []
 feature_keys = list(feature_ranges.keys())
@@ -80,53 +80,7 @@ features = np.array([feature_values])
 
 # 预测
 if st.button("Predict"):
-    predicted_class = model.predict(features)[0]
-    predicted_proba = model.predict_proba(features)[0]
-    probability = predicted_proba[predicted_class] * 100
-
-    # 显示预测结果
-    text = f"Based on feature values, predicted possibility of hemorrhage after lung transplantation is {probability:.2f}%"
-    fig, ax = plt.subplots(figsize=(8, 1))
-    ax.text(0.5, 0.5, text, fontsize=16, ha='center', va='center', fontname='Times New Roman', transform=ax.transAxes)
-    ax.axis('off')
-    plt.savefig("prediction_text.png", bbox_inches='tight', dpi=300)
-    st.image("prediction_text.png")
-
-    # SHAP 解释
-    # 提取底层模型（支持 pipeline 或直接模型）
-    def get_tree_model(model):
-        from sklearn.pipeline import Pipeline
-        if isinstance(model, Pipeline):
-            return model.named_steps['clf']
-        return model
-
-    tree_model = get_tree_model(model)
-    explainer = shap.TreeExplainer(tree_model)
-    
-    
-    shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_keys))
-    
-    if isinstance(shap_values, list) and len(shap_values) > 1:
-        shap_values_for_display = shap_values[1]  
-        base_value = explainer.expected_value[1]
-    elif isinstance(shap_values, list) and len(shap_values) == 1:
-        shap_values_for_display = shap_values[0]
-        base_value = explainer.expected_value[0]
-    else:
-        shap_values_for_display = shap_values
-        base_value = explainer.expected_value
-
-    # 将特征名替换为缩写
-    feature_keys_abbr = [feature_abbr.get(f, f) for f in feature_keys]  # 将特征名替换为缩写
-
-    # 生成 SHAP 力图
-    shap.initjs()
-    shap_fig = shap.plots.force(
-        base_value,  # 基准值
-        shap_values_for_display,  # SHAP 值
-        pd.DataFrame([feature_values], columns=feature_keys_abbr),  # 使用缩写作为列名
-        matplotlib=True,
-        show=False  # 不自动显示图形
-    )
-    
-    st.pyplot(shap_fig)
+    prediction = model.predict(features)[0]
+    Predict_proba = model.predict_proba(features)[:, 1][0]
+    # 输出概率
+    st.write(f"Based on feature values, predicted possibility of hemorrhage after lung transplantation is :  {'%.2f' % float(Predict_proba * 100) + '%'}")
